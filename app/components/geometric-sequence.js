1957181320b1c1cc76cf3fbd9464e434db348a45
Jeffrey Canty <hello@jeffrey.engineer>
2017-12-14 15:11:25 -0600

    Complete `Geometric Sequence` exercise.

--- app/components/geometric-sequence.js
+++ app/components/geometric-sequence.js
@@ -1,13 +1,12 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
-
-  init: function(){
+  init: function() {
     this.initializeSequence();
     this._super();
   },
 
-  initializeSequence: function(){
+  initializeSequence: function() {
     //geometric sequence of ^2: 1, 2, 4, 8, 16, etc...
     this.set("geometricSequence", Ember.A([1, 2, 4]));
   },
@@ -17,22 +16,29 @@ export default Ember.Component.extend({
   //set in component initializer
   geometricSequence: null,
 
-  //reverse sequence is rendered in geometric-sequence.hbs
-  reverseSequence: Ember.computed('geometricSequence', 'geometricSequence.@each', function() {
-    var sequence = this.get("geometricSequence");
-    var reverse = sequence;
-
-    //reverse the array here
-
-    return reverse;
+  // Clone `geometricSequence`, sorting elements from largest to smallest:
+  reversedSequence: Ember.computed.sort("geometricSequence", function(a, b) {
+    return a < b ? 1 : -1;
   }),
 
-  actions:{
-    updateSequence: function(){
-      var sequence = this.get("geometricSequence");
-      alert("Trying to update sequence (You can remove this alert)");
+  // Clone `reversedSequence`, filtering items to only the first ten, which is
+  // consumed in hbs template:
+  filteredSequence: Ember.computed.filter("reversedSequence", function(i, idx) {
+    return idx < 10;
+  }),
 
-      //Modify the sequence here
+  actions: {
+    updateSequence: function() {
+      let condition = Math.pow(2, 15);
+      let sequence = this.get("geometricSequence");
+      let prev = sequence.pop();
+
+      // If condition is met, re-initialize array:
+      if (prev === condition) {
+        return this.initializeSequence();
+      }
+      // Otherwise, set geometricSequence to a newly spread array:
+      return this.set("geometricSequence", [...sequence, prev, prev * 2]);
     }
   }
 });
